1f47cf704bf8b625dd6da0da51b5195e6158d59a
Aakash <Admin@DESKTOP-8V0Q5AG>
2025-09-04 13:29:12 +0530

    connected local project with GitHub

---
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Enqueue parent and child theme styles
+ */
+function hello_elementor_child_enqueue_styles() {
+    // Parent style
+    wp_enqueue_style(
+        'hello-elementor-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    // Child style
+    wp_enqueue_style(
+        'hello-elementor-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('hello-elementor-style'),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_styles');
